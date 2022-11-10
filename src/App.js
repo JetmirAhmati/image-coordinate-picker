8113ed9a5fce9d15757b76c0d20e0e3cb873c93e
Jetmir Ahmati <j.ahmati@reply.de>
2022-11-10 12:10:46 +0100

    add project

---
+++ src/App.js
@@ -0,0 +1,105 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useRef, useState } from 'react'
+
+
+const loadImage = (setImageDimensions, imageUrl) => {
+  if (!imageUrl) return;
+
+  const img = new Image();
+  img.src = imageUrl;
+
+  img.onload = () => {
+    setImageDimensions({
+      height: img.height,
+      width: img.width
+    });
+
+    console.log({ height: img.height, width: img.width });
+  };
+
+  img.onerror = (err) => {
+    console.log("img error");
+    console.error(err);
+  };
+};
+
+const App = () => {
+  const [imageDimensions, setImageDimensions] = useState({}),
+    [imageUrl, setImageUrl] = useState(null), //"https://picsum.photos/200/300";
+    hiddenFileInput = useRef(null);
+  const imageRef = useRef(null);
+  const [cordinatePickerList, setCordinatePickerList] = useState([]);
+  const [counter, setCounter] = useState(0)
+
+  const handleClick = (event) => {
+    hiddenFileInput.current.click();
+  };
+
+  const handleChange = event => {
+    const fileUploaded = event.target.files[0],
+      imageSrc = URL.createObjectURL(fileUploaded)
+    setImageUrl(imageSrc)
+  };
+
+  useEffect(() => {
+    loadImage(setImageDimensions, imageUrl);
+    console.log(imageRef.current)
+  }, [imageUrl]);
+
+  useEffect(() => {
+    console.log("cordinatePickerList", cordinatePickerList);
+  }, [cordinatePickerList])
+
+  const handleImageClick = (event) => {
+    const tolerance = 5;
+    const target = event.target;
+    const { clientX, clientY } = event;
+    const rect = target.getBoundingClientRect();
+
+    const clickXPos = clientX - rect.left - tolerance;
+    const clickYPos = clientY - rect.top - tolerance;
+    setCounter(counter + 1);
+    const hotpsotId = `hotspotid-${counter}`;
+    const clickedCord = {hotpsotId, clickXPos, clickYPos };
+
+    setCordinatePickerList([...cordinatePickerList, clickedCord])
+  }
+
+  return (
+    <div className="App">
+      <img src={logo} width="45px" />
+      <p>Image Cordinate Picker</p>
+      <button onClick={handleClick}> Upload a image </button>
+      <input type="file" ref={hiddenFileInput} onChange={handleChange} style={{ display: 'none' }} />
+
+      {imageUrl &&
+        <div className='container'>
+          <div>
+            <div className='image-placeholder'>
+              <img src={imageUrl} ref={imageRef} onClick={(event) => handleImageClick(event)} />
+              {cordinatePickerList.map((item, i) => (
+                <div key={i} id={item.hotpsotId} className="hotpsot" style={{ "left": item.clickXPos, "top": item.clickYPos }}></div>
+              ))}
+            </div>
+          </div>
+        </div>}
+
+      {Object.keys(imageDimensions).length > 0 &&
+        <>
+          <p>
+            <b>Width:</b>{imageDimensions.width}{" "}
+            <b>Height:</b>{imageDimensions.height}{" "}
+          </p>
+          <div>
+            {cordinatePickerList.map((item, i) => (
+              <div data-id={item.hotpsotId} key={i}>hotpsotId: <b>{item.hotpsotId}</b> Coordinates: <b>{item.clickXPos}, {item.clickYPos}</b></div>
+            ))}
+          </div>
+        </>
+      }
+    </div>
+  );
+}
+
+export default App;
