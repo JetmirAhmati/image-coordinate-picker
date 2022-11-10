1cd6275a5508dc6ac7f81e0080a6c5ba01ef5dc6
Jonida693 <jonida.doci@rws-solutions.com>
2022-11-10 17:02:23 +0100

    added the remove hotspot  button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useEffect, useRef, useState } from 'react'
-
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
 const loadImage = (setImageDimensions, imageUrl) => {
   if (!imageUrl) return;
@@ -12,7 +11,7 @@ const loadImage = (setImageDimensions, imageUrl) => {
   img.onload = () => {
     setImageDimensions({
       height: img.height,
-      width: img.width
+      width: img.width,
     });
 
     console.log({ height: img.height, width: img.width });
@@ -30,26 +29,26 @@ const App = () => {
     hiddenFileInput = useRef(null);
   const imageRef = useRef(null);
   const [cordinatePickerList, setCordinatePickerList] = useState([]);
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
 
   const handleClick = (event) => {
     hiddenFileInput.current.click();
   };
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     const fileUploaded = event.target.files[0],
-      imageSrc = URL.createObjectURL(fileUploaded)
-    setImageUrl(imageSrc)
+      imageSrc = URL.createObjectURL(fileUploaded);
+    setImageUrl(imageSrc);
   };
 
   useEffect(() => {
     loadImage(setImageDimensions, imageUrl);
-    console.log(imageRef.current)
+    console.log(imageRef.current);
   }, [imageUrl]);
 
   useEffect(() => {
     console.log("cordinatePickerList", cordinatePickerList);
-  }, [cordinatePickerList])
+  }, [cordinatePickerList]);
 
   const handleImageClick = (event) => {
     const tolerance = 5;
@@ -61,45 +60,82 @@ const App = () => {
     const clickYPos = Math.round(clientY - rect.top - tolerance);
     setCounter(counter + 1);
     const hotpsotId = `hotspotid-${counter}`;
-    const clickedCord = {hotpsotId, clickXPos, clickYPos };
+    const clickedCord = { hotpsotId, clickXPos, clickYPos };
 
-    setCordinatePickerList([...cordinatePickerList, clickedCord])
-  }
+    setCordinatePickerList([...cordinatePickerList, clickedCord]);
+  };
+
+  const removehotspots = (hotspotId) => {
+    console.log(hotspotId);
+    const newCordinatePicerList = cordinatePickerList.filter((hotspot) => {
+      return hotspot.hotpsotId !== hotspotId;
+    });
+
+    console.log(newCordinatePicerList);
+    setCordinatePickerList(newCordinatePicerList);
+  };
 
   return (
     <div className="App">
       <img src={logo} width="45px" />
       <p>Image Cordinate Picker</p>
       <button onClick={handleClick}> Upload a image </button>
-      <input type="file" ref={hiddenFileInput} onChange={handleChange} style={{ display: 'none' }} />
+      <input
+        type="file"
+        ref={hiddenFileInput}
+        onChange={handleChange}
+        style={{ display: "none" }}
+      />
 
-      {imageUrl &&
-        <div className='container'>
+      {imageUrl && (
+        <div className="container">
           <div>
-            <div className='image-placeholder'>
-              <img src={imageUrl} ref={imageRef} onClick={(event) => handleImageClick(event)} />
+            <div className="image-placeholder">
+              <img
+                src={imageUrl}
+                ref={imageRef}
+                onClick={(event) => handleImageClick(event)}
+              />
               {cordinatePickerList.map((item, i) => (
-                <div key={i} id={item.hotpsotId} className="hotpsot" style={{ "left": item.clickXPos, "top": item.clickYPos }}></div>
+                <div
+                  key={i}
+                  id={item.hotpsotId}
+                  className="hotpsot"
+                  style={{ left: item.clickXPos, top: item.clickYPos }}
+                ></div>
               ))}
             </div>
           </div>
-        </div>}
+        </div>
+      )}
 
-      {Object.keys(imageDimensions).length > 0 &&
+      {Object.keys(imageDimensions).length > 0 && (
         <>
           <p>
-            <b>Width:</b>{imageDimensions.width}{" "}
-            <b>Height:</b>{imageDimensions.height}{" "}
+            <b>Width:</b>
+            {imageDimensions.width} <b>Height:</b>
+            {imageDimensions.height}{" "}
           </p>
           <div>
             {cordinatePickerList.map((item, i) => (
-              <div data-id={item.hotpsotId} key={i}>hotpsot: <b>{item.hotpsotId}</b> Coordinates: <b>{item.clickXPos}, {item.clickYPos}</b></div>
+              <div className="cordinates" data-id={item.hotpsotId} key={i}>
+                hotpsot: <b>{item.hotpsotId}</b> Coordinates:{" "}
+                <b>
+                  {item.clickXPos}, {item.clickYPos}
+                </b>
+                <button
+                  className=" remove-btn"
+                  onClick={() => removehotspots(item.hotpsotId)}
+                >
+                  Remove
+                </button>
+              </div>
             ))}
           </div>
         </>
-      }
+      )}
     </div>
   );
-}
+};
 
 export default App;
